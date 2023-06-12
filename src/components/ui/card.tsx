0034bfe3f6244da6dec2377d0087a6667747cadc
Felix <felixzarate14@gmail.com>
2023-06-12 15:13:10 -0400

    feat: add Service section on landing page

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -60,7 +60,7 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  <div ref={ref} className={cn("", className)} {...props} />
 ));
 CardContent.displayName = "CardContent";
 
@@ -68,11 +68,7 @@ const CardFooter = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(" flex items-center p-6 pt-0", className)}
-    {...props}
-  />
+  <div ref={ref} className={cn(" flex p-6 pt-0", className)} {...props} />
 ));
 CardFooter.displayName = "CardFooter";
 
